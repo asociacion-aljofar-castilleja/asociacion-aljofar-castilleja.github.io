77124f18d6e4c39d6c35eb81dbefdf8bfdc370e1
Antonio Gonzalez <casado84@gmail.com>
2025-11-06 11:06:40 +0100

    cambios para ruta de scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://asociacionaljofarcastilleja.es',
     base: '/', // raíz del dominio
+    vite: {
+        build: { assetsInlineLimit: 0 },
+        css: { preprocessorOptions: { sass: { quietDeps: true } } },
+    },
 });
